8ba00f4d0a7b12403d4781a09e10a2bc530bb484
Sergey Kuznetsov <sergeykuzn@gmail.com>
2023-03-24 08:44:35 +0100

    Added CartService

---
+++ src/src/CartingService/NetMentoringAdvanced.CartingService/BLL/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using NetMentoringAdvanced.CartingService.Domain;
+
+namespace NetMentoringAdvanced.CartingService.BLL
+{
+    public interface ICartService
+    {
+        IEnumerable<CartItem> GetItems(Guid cartId);
+
+        void AddItem(Guid cartId, CartItem item);
+
+        void RemoveItem(Guid cartId, int itemId);
+    }
+}
